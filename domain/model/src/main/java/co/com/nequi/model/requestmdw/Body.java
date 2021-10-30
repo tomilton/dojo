83af6348093d1698bf836543fac0875101a37e5b
Milton <tomilton@hotmail.com>
2021-10-30 10:28:53 -0500

    Request mdw

---
+++ domain/model/src/main/java/co/com/nequi/model/requestmdw/Body.java
@@ -0,0 +1,13 @@
+package co.com.nequi.model.requestmdw;
+
+public class Body {
+    private Object any;
+
+    public Object getAny() {
+        return any;
+    }
+
+    public void setAny(Object any) {
+        this.any = any;
+    }
+}
